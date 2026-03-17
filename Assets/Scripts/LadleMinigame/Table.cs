f158a28bec3b6da3da2137ac68985ec5da74b4c3
cdburner <davidwright66@googlemail.com>
2026-03-17 17:28:14 +0000

    ladle game start

---
+++ Assets/Scripts/LadleMinigame/Table.cs
@@ -0,0 +1,24 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    //rotation 22 and -22
+    public float maxLeftRotation;
+    public float maxRightRotation;
+    public float maxRotationSpeed;
+    public float minRotationSpeed;
+    public float maxWaitTime;
+    public float minWaitTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
